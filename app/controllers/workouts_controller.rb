a0b0ad8c8f0638e7ffa165ef46a8a0ef7e043425
Rachelle Herrera <rachelle.herera@gmail.com>
2015-10-06 23:49:33 -0700

    added workouts controller

---
+++ app/controllers/workouts_controller.rb
@@ -0,0 +1,19 @@
+class WorkoutsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'workouts/index'
+
+  get 'workouts/new'
+
+  get 'workouts/edit'
+
+  get 'workouts/show'
+
+  get 'workouts/update'
+
+  get 'workouts/destroy'
+
   root 'static_pages#index'
 
     get '/login', to: 'sessions#new'
