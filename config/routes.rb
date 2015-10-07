49a18055b3fcb733f4dcbe263fbaa7a63aa3deda
Rachelle Herrera <rachelle.herera@gmail.com>
2015-10-06 22:28:53 -0700

    added controller for photos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'photos/index'
+
+  get 'photos/new'
+
+  get 'photos/show'
+
+  get 'photos/edit'
+
+  get 'photos/update'
+
+  get 'photos/destroy'
+
   root 'static_pages#index'
 
    get '/login', to: 'sessions#new'
