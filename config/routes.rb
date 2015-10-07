8b259f49fc42959a49e3f5202332943e302cf720
Rachelle Herrera <rachelle.herera@gmail.com>
2015-10-07 00:02:39 -0700

    created entity relationship with comments and photos

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,16 @@ Rails.application.routes.draw do
 
   root 'static_pages#index'
 
-    get '/login', to: 'sessions#new'
-   
     delete '/logout', to: 'sessions#destroy'
 
+    get '/login', to: 'sessions#new'
+    
     resources :sessions
     resources :users
     resources :workouts
    
     resources :photos do 
       resources :comments
-    end
+    end 
    
 end
