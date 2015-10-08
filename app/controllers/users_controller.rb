66409baf63d1939a837ae57b810d16898b92c617
Rachelle Herrera <rachelle.herera@gmail.com>
2015-10-07 21:08:12 -0700

    refactored the user controller for avatar to be uploaded

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.create(user_params)
     if @user.save 
       session[:user_id] = @user.id
       redirect_to user_path(current_user)
