dfa74cfcd111330ecca2e0bdb9085e72338d4b86
Rachelle Herrera <rachelle.herera@gmail.com>
2015-10-08 23:23:02 -0700

    added methods for meds controller

--- app/controllers/meds_controller.rb
+++ app/controllers/meds_controller.rb
@@ -1,2 +1,40 @@
 class MedsController < ApplicationController
+
+  def index 
+    @meds = Med.all
+  end 
+
+  def new
+    @med = current_user.meds.new
+  end
+
+  def create 
+    @med = current_user.meds.create(med_params)
+  end
+
+  def edit 
+    @med = Med.find(params[:id])
+  end
+
+  def update
+    if @med.update(med_params)
+      flash[:success] = "Med updated" 
+      redirect_to meds_path 
+    else 
+      render 'edit'
+    end
+  end
+
+  def destroy 
+    @med = Med.find(params[:id])
+    @med.destroy 
+    redirect_to users_path(current_user)
+  end
+
+  private 
+
+  def med_params 
+    params.require(:med).permit(:name, :description)
+  end
+
 end
